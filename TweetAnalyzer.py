b71eced471e246303387db517f35082ff68c1662
jtgetsin <jtgetsin@uncc.edu>
2017-04-02 19:28:12 -0400

    Update TweetAnalyzer.py

--- TweetAnalyzer.py
+++ TweetAnalyzer.py
@@ -4,33 +4,35 @@ import operator
 from collections import Counter
 import datetime
 import re
+import sys
 
+#use to translate tweets for printing in shell
+non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode +1), 0xfffd)
 #You'll need to replace these values with your own twitter app, start at https://apps.twitter.com
 #After creating a new app find the needed creditials at 'Keys and Access Tokens' tab
-ACCESS_TOKEN = "at"
-ACCESS_SECRET = "as"
-CONSUMER_KEY = "ck"
-CONSUMER_SECRET = "cs"
+ACCESS_TOKEN = "827582834122563585-Z6iFpPaDqmAXKaeLiqNX23hwpoe2ZCk"
+ACCESS_SECRET = "dzxYhH166vbRChEITWISCO3S6j5qW2Fi0h6l25BQLso8P"
+CONSUMER_KEY = "v0wMSuWKid0qe2tf3N9e1lkcx"
+CONSUMER_SECRET = "SXJXuvzBNTcuhbIzjAmgkpp8KnRpN8K3bYovd4KexazWuo7uJG"
 #connect to twitter API
 twitter = Twitter(auth=OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
 
 #simple user input for twitter handle
 twitterHandle = input("Enter twitter handle for analysis: ")
-
+#tweet1: stores all tweets with detailed information
 tweet1 = []
+#tweet2: stores just tweet text
 tweet2 = []
-tweetAnalysisVolume = 0
-
 #variable for twitter query instance(s)
 JSONroot = twitter.statuses.user_timeline(screen_name=twitterHandle,count=1)
 tweet1.extend(JSONroot)
-#Oldest tweet id, used to pull more than 200
+#oldest: Oldest tweet id used to keep track when pulling over 200 tweets
 oldest = tweet1[0]['id']
-earlyTweet = lunchTweet = eveningTweet = lateTweet = 0
+earlyTweet = lunchTweet = eveningTweet = lateTweet = tweetAnalysisVolume = 0
 print("\nInfo on " + " historical tweets from: " + twitterHandle + "\n")
 while len(JSONroot) > 0:
     JSONroot = twitter.statuses.user_timeline(screen_name=twitterHandle, count=200,max_id=oldest)
-    #number of tweets pulled out of 200 i.e. if user has 6 tweets, check = 6
+    #tweetsPulled: number of tweets pulled out of 200 i.e. if user has 206 tweets, 1st pull is 200, 2nd is 6
     tweetsPulled = len(JSONroot)
     tweet1.extend(JSONroot)
     oldest = tweet1[-1]['id'] - 1
@@ -66,7 +68,7 @@ while len(JSONroot) > 0:
         tweetAnalysisVolume += 1
         tweet2.append(JSONroot[i]['text'])
         print(createdAt)
-        print(tweet + "\n")
+        print(tweet.translate(non_bmp_map))
 
 
 #tweet percentage stats and prints
@@ -88,12 +90,12 @@ ignore = ['rt', 'retweet', 'a', 'an', 'am', 'of', 'that', 'with', 'will', 'at', 
           'came', 'know', 'girl', 'boy', 'face', 'good', 'best', 'never', 'has', 'have', 'had', 'up', 'new', 'make',
           'so', 'out', 'all', 'more', 'president', 'news', 'their', 'there', 'must', 'ok', 'last', 'first',
           'get', 'lol', 'but', 'see', 'saw', 'about', 'dont', 'do', 'amp', 'im', 'these', 'would', 'can', 'cant',
-          'when', 'why', 'take', 'ive', 'or', 'than', 'them', 'eat', 'eating', 'pm', 'am', 'b', 'c', 'd', 'e', 'f', 'g',
+          'when', 'why', 'take', 'ive', 'or', 'than', 'them', 'eat', 'eating', 'pm', 'b', 'c', 'd', 'e', 'f', 'g',
           'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'were',
           'where', 'tonight', 'tomorrow', 'week', 'thank', 'thanks', 'man', 'woman', 'from', 'like', 'come', 'just',
           'today', 'now', 'one', 'tv', 'eat', 'back', 'much', 'going', 'went', 'want', 'another', 'got', 'still',
           'yall', 'whats', 'how', 'lets', 'lot', 'been', 'here', 'hear', 'way', 'some', 'little', 'big', 'ill', 'then',
-          'think', 'feel', 'let', 'said', 'that', 'thats', 'because', 'off', 'on', 'time', 'ever', 'who', 'ur', 'right',
+          'think', 'feel', 'let', 'said', 'that', 'thats', 'because', 'off', 'time', 'ever', 'who', 'ur', 'right',
           'someone', 'oh', 'people', 'life', 'gonna', 'being', 'hello', 'hi', 'havent', 'goodbye', 'well']
 
 allTweets = ''.join(tweet2)
@@ -104,14 +106,6 @@ allTweets = allTweets.lower()
 allTweets = allTweets.split()
 termsUsed = [term for term in allTweets if term not in ignore]
 count.update(termsUsed)
-print(count.most_common(10))
-
-
-
-
-
-
-
-
+print("10 of this user's favorite words: " + str(count.most_common(10)))
 
 
